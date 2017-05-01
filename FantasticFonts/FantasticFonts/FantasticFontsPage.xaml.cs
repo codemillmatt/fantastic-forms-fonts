ff5604cf10d8c45df5c250f4ab96205058fe6b4e
Matt Soucoup <msoucoup@codemilltech.com>
2017-05-01 06:17:22 -0500

    Demo app

---
+++ FantasticFonts/FantasticFonts/FantasticFontsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace FantasticFonts
+{
+	public partial class FantasticFontsPage : ContentPage
+	{
+		public FantasticFontsPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
